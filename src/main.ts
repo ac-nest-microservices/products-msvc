03c854869f3ba376aa3e8abe2c28225ab5d20b53
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2024-06-24 17:10:57 -0600

    Add env config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
+import { config } from './config/envs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(config.port);
+  console.log(`Application is running on port ${config.port}`);
 }
 bootstrap();
